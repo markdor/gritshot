b3dedcd1e503797d1c1d277e1663a74fa74a81b3
markdor <markus.doerfler@proton.me>
2026-03-17 20:07:00 +0100

    feat: added favicons and logos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,12 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="GritShot" />
+	<link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
 {@render children()}
