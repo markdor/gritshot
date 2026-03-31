a30a1b8cecc9e1fe94ae4a21caa27ea7d8109c45
markdor <markus.doerfler@proton.me>
2026-03-31 19:13:14 +0200

    build: coverage as json

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 	test: {
 		coverage: {
 			provider: 'v8',
-			reporter: ['text', 'lcov', 'html'],
+			reporter: ['text', 'lcov', 'html', 'json-summary'],
 			include: ['src/**/*.{ts,svelte}'],
 			exclude: ['src/**/*.test.ts', 'src/**/*.spec.ts']
 		},
